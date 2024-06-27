5c8ee010c67f0ce92bbfada2e7fa84cd03dd1706
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-06-27 17:28:15 -0300

    refactor: Update Client entity to include phone field

--- consumer/client/src/client.entity.ts
+++ consumer/client/src/client.entity.ts
@@ -17,11 +17,14 @@ export class Client {
   @Column()
   name: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: false })
   email: string;
 
   @Column({ default: true })
   isActive: boolean;
+
+  @Column({ nullable: true })
+  phone: string;
 
   @OneToMany(() => Order, (order) => order.client)
   orders: Order[];
