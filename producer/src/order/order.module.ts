d75447c523459fdba23a2c247bb928133769ee7d
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-06-28 19:52:09 -0300

    chore: Add PassportModule and JwtModule for JWT authentication in OrderModule

--- producer/src/order/order.module.ts
+++ producer/src/order/order.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from 'src/strategy/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1d' } }),
     ClientsModule.register([
       {
         name: 'ORDER_SERVICE',
@@ -20,6 +25,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     ]),
   ],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, JwtStrategy],
 })
 export class OrderModule {}
