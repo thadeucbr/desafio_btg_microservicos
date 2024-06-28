2399943420d9010ad8cc88ff42a8960845ece1a8
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-06-28 19:51:34 -0300

    refactor: Update Swagger configuration to include bearer authentication

--- producer/src/main.ts
+++ producer/src/main.ts
@@ -6,7 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').build();
+  const config = new DocumentBuilder()
+  .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+  .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
