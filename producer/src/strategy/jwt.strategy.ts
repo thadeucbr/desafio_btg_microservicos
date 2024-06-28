e0a239ff7e46a5444f1c489f22a4bc4865542e81
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-06-28 19:52:00 -0300

    refactor: Add JwtStrategy for handling JWT authentication

---
+++ producer/src/strategy/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+interface JwtPayload {
+  sub: string;
+  role: string;
+}
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secret',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    console.log(payload)
+    return payload;
+  }
+}
