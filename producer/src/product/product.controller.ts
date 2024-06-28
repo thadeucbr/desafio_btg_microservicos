79d1f3d70dd1f35fe504b8594317021830a2f8b9
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-06-28 09:47:31 -0300

    refactor: Update product controller and service to add, update, and delete product endpoints

--- producer/src/product/product.controller.ts
+++ producer/src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, NotFoundException, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDto } from './product.dto';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -9,15 +9,46 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post('add-product')
+  @HttpCode(202)
   @ApiOperation({ summary: 'Add a product' })
-  @ApiResponse({ status: 201, description: 'Product added' })
+  @ApiResponse({ status: 202, description: 'Product added' })
   @ApiBody({ type: ProductDto })
   addProduct(@Body() product: ProductDto) {
     return this.productService.addProduct(product);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiResponse({ status: 200, description: 'Return all products' })
   getProducts() {
     return this.productService.getProducts();
+  }
+
+  @Post('update-product/:id')
+  @HttpCode(202)
+  @ApiOperation({ summary: 'Update a product' })
+  @ApiResponse({ status: 202, description: 'Product updated' })
+  @ApiBody({ type: ProductDto })
+  updateProduct(@Param() data: { id: number; product: ProductDto }) {
+    return this.productService.updateProduct(data.id, data.product);
+  }
+
+  @Post('delete-product/:id')
+  @HttpCode(202)
+  @ApiOperation({ summary: 'Delete a product' })
+  @ApiResponse({ status: 202, description: 'Product deleted' })
+  deleteProduct(@Param() id: number) {
+    return this.productService.deleteProduct(id);
+  }
+
+  @Get('product/:id')
+  @ApiOperation({ summary: 'Get a product' })
+  @ApiResponse({ status: 200, description: 'Return a product' })
+  async getProduct(@Param('id') id: number) {
+    const product = await this.productService.getProduct(id).toPromise();
+    if (!product) {
+      throw new NotFoundException();
+    }
+    return product;
   }
 }
--- producer/src/product/product.service.ts
+++ producer/src/product/product.service.ts
@@ -18,4 +18,24 @@ export class ProductService {
   getProducts() {
     return this.rabbitClient.send({ cmd: 'fetch-products' }, {}).pipe(timeout(5000));
   }
+
+  updateProduct(id: number, product: ProductDto) {
+    this.rabbitClient.emit('update-product', { id, product });
+
+    return {
+      message: 'Product updated!',
+    };
+  }
+
+  deleteProduct(id: number) {
+    this.rabbitClient.emit('delete-product', id);
+
+    return {
+      message: 'Product deleted!',
+    };
+  }
+
+  getProduct(id: number) {
+    return this.rabbitClient.send({ cmd: 'fetch-product' }, id).pipe(timeout(5000));
+  }
 }
